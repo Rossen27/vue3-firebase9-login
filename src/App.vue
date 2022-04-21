4b0d672adb0a171d87a9bca2d080879a2bdd8dfc
RossenLin <97585139+Rossen27@users.noreply.github.com>
2022-04-21 14:39:25 +0800

    into

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <nav>
       <router-link to="/"> Home </router-link> |
       <router-link to="/feed"> Feed </router-link> |
-      <router-link to="/register"> Register </router-link> |
+      <!-- <router-link to="/register"> Register </router-link> | -->
       <router-link to="/sign-in"> Login </router-link> |
       <button @click="handleSignOut" v-if="isLoggedIn">Sign Out</button>
     </nav>
